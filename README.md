45b718cf307381926ce06ccac7f0835d0ae9a6f6
Chris Ferdinandi <hello@gomakethings.com>
2013-10-10 20:26:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# HTML Minify
+Minify your HTML output in WordPress.
+
+
+## How It Works
+Getting started with HTML Minify is as simple as installing a plugin:
+
+1. Upload the `html-minify` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the Plugins menu in WordPress.
+3. Pat yourself on the back.
+
+Seriously, you're done. Nice work! You change what gets minified under "Settings" in the Admin Dashboard.
+
+
+## Changelog
+* v1.0 (October 10, 2013)
+  * Initial release.
+
+
+## License
+HTML Minify is free to use under the [MIT License](http://gomakethings.com/mit/).
---
+++ html-minify.php
@@ -0,0 +1,21 @@
+<?php
+
+/* ======================================================================
+
+    Plugin Name: HTML Minify
+    Plugin URI: http://github.com/cferdinandi/html-minify
+    Description: Minify your HTML output in WordPress.
+    Version: 1.0
+    Author: Chris Ferdinandi
+    Author URI: http://gomakethings.com
+    License: MIT
+
+    Forked from DVS.
+    http://www.intert3chmedia.net/2011/12/minify-html-javascript-css-without.html
+
+ * ====================================================================== */
+
+require_once( dirname( __FILE__) . '/html-minify-options.php' );
+require_once( dirname( __FILE__) . '/html-minify-process.php' );
+
+?>
